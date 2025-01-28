388c2035ac11b7bd185dd15de198a529c4f95048
Namasivayam M <iamnamachu40@gmail.com>
2025-01-28 13:57:21 +0530

    Merge feature1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyB2F9tF8h40lgYslH1YfZylHlgSnhuRXJI";
+export const GOOGLE_API_KEY = "";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
